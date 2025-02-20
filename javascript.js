de49af8c86132587d43b57ff283d95fbb008bb5c
Ipecac <cacem@pm.me>
2025-02-20 13:24:35 -0500

    add basic artithmetic functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let number1 = 0;
+let number2 = 0;
+let operator = '';
+
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
